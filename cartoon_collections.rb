a1bc558455b2a3c23883caee08aee7e96de1be99
Stephanie M Segura <stephaniemsegura09@gmail.com>
2020-08-07 03:59:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,6 +2,7 @@ require 'pry'
 def greet_characters(array)
   array.each do |element|
     puts "Hello #{element}!"
+    # it "prints out a custom greeting for each name in the provided array"
 end
 end
   
